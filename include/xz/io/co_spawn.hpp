7483714c6ac15f6a4d736ce7e729d9a647bfc773
yxzchen <chenyxmr@gmail.com>
2025-12-21 00:10:41 +0800

    sync

--- include/xz/io/co_spawn.hpp
+++ include/xz/io/co_spawn.hpp
@@ -1,6 +1,30 @@
 #pragma once
 
 #include <xz/io/awaitable.hpp>
-#include <xz/io/io_context.hpp>
+#include <xz/io/executor.hpp>
 
-namespace xz::io {}
+namespace xz::io {
+
+/// Start an awaitable on the given executor (cold coroutine model).
+///
+/// Ownership of the coroutine frame is detached from the passed awaitable:
+/// the frame will be destroyed at final_suspend.
+template <typename T>
+void co_spawn(executor ex, awaitable<T> a) {
+  auto h = a.coro_;
+  a.coro_ = {};
+
+  h.promise().set_executor(ex);
+  h.promise().detach();
+
+  ex.post([h, ex]() mutable {
+    detail::executor_guard g{ex};
+    try {
+      h.resume();
+    } catch (...) {
+      // Detached mode: swallow exceptions
+    }
+  });
+}
+
+}  // namespace xz::io
