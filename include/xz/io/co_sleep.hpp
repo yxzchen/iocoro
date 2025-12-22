3be81a7a6590003e1afcc1d611d911a9e3cbb5a3
yxzchen <chenyxmr@gmail.com>
2025-12-22 13:42:41 +0800

    sync

--- include/xz/io/co_sleep.hpp
+++ include/xz/io/co_sleep.hpp
@@ -3,8 +3,8 @@
 #include <xz/io/assert.hpp>
 #include <xz/io/awaitable.hpp>
 #include <xz/io/executor.hpp>
+#include <xz/io/steady_timer.hpp>
 #include <xz/io/this_coro.hpp>
-#include <xz/io/timer_handle.hpp>
 #include <xz/io/use_awaitable.hpp>
 
 #include <chrono>
@@ -22,14 +22,9 @@ inline auto co_sleep(std::chrono::steady_clock::duration d) -> awaitable<void> {
   auto ex = co_await this_coro::executor;
   XZ_ENSURE(ex, "co_sleep: requires a bound executor");
 
-  using namespace std::chrono;
-  auto ms = ceil<milliseconds>(d);
-  if (ms.count() < 0) {
-    ms = milliseconds{0};
-  }
-
-  auto th = ex.schedule_timer(ms, [] {});
-  co_await th.async_wait(use_awaitable);
+  steady_timer t{ex};
+  t.expires_after(d);
+  co_await t.async_wait(use_awaitable);
 }
 
 }  // namespace xz::io
