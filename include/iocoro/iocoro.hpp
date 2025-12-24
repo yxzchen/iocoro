bfe12ca7b922220c7757c5a2bea6c76c23d64710
yxzchen <chenyxmr@gmail.com>
2025-12-24 23:50:01 +0800

    sync

--- include/iocoro/iocoro.hpp
+++ include/iocoro/iocoro.hpp
@@ -1,20 +1,22 @@
 #pragma once
 
+#include <iocoro/error.hpp>
+#include <iocoro/expected.hpp>
+
 #include <iocoro/awaitable.hpp>
-#include <iocoro/basic_socket.hpp>
 #include <iocoro/co_sleep.hpp>
 #include <iocoro/co_spawn.hpp>
-#include <iocoro/detached.hpp>
-#include <iocoro/error.hpp>
 #include <iocoro/executor.hpp>
-#include <iocoro/expected.hpp>
 #include <iocoro/io_context.hpp>
-#include <iocoro/ip.hpp>
-#include <iocoro/socket_option.hpp>
-#include <iocoro/steady_timer.hpp>
 #include <iocoro/this_coro.hpp>
+#include <iocoro/work_guard.hpp>
+
+#include <iocoro/detached.hpp>
+#include <iocoro/steady_timer.hpp>
 #include <iocoro/timer_handle.hpp>
 #include <iocoro/use_awaitable.hpp>
 #include <iocoro/when_all.hpp>
 #include <iocoro/when_any.hpp>
-#include <iocoro/work_guard.hpp>
+
+#include <iocoro/basic_socket.hpp>
+#include <iocoro/ip.hpp>
--- include/iocoro/ip.hpp
+++ include/iocoro/ip.hpp
@@ -2,10 +2,10 @@
 
 #include <iocoro/ip/address.hpp>
 #include <iocoro/ip/resolver.hpp>
+
 #include <iocoro/ip/tcp/acceptor.hpp>
 #include <iocoro/ip/tcp/endpoint.hpp>
 #include <iocoro/ip/tcp/socket.hpp>
+
 #include <iocoro/ip/udp/endpoint.hpp>
 #include <iocoro/ip/udp/socket.hpp>
-
-namespace iocoro {}
