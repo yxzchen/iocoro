752840be6d12b4e0550a1046a7a06897cec8d07f
yxzchen <chenyxmr@gmail.com>
2025-12-25 01:27:40 +0800

    sync

--- include/iocoro/ip/resolver.hpp
+++ include/iocoro/ip/resolver.hpp
@@ -2,7 +2,6 @@
 
 #include <iocoro/detail/ip/resolver_impl.hpp>
 #include <iocoro/io_context.hpp>
-#include <iocoro/ip/endpoint.hpp>
+#include <iocoro/ip/tcp/endpoint.hpp>
 
-namespace iocoro {}
 namespace iocoro::ip {}
