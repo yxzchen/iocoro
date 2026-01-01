04a84e698f00f6cf0224a40eb47b9ca460a57c26
yxzchen <chenyxmr@gmail.com>
2026-01-01 14:45:26 +0800

    sync

--- include/iocoro/io_context.hpp
+++ include/iocoro/io_context.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <iocoro/detail/io_context_impl.hpp>
+#include <iocoro/io_executor.hpp>
+
 #include <chrono>
 #include <cstdint>
 #include <functional>
@@ -7,33 +10,27 @@
 
 namespace iocoro {
 
-class io_executor;
-
-namespace detail {
-class io_context_impl;
-}  // namespace detail
-
 /// The execution context for asynchronous I/O operations
 class io_context {
  public:
-  io_context();
-  ~io_context();
+  io_context() : impl_(std::make_unique<detail::io_context_impl>()) {}
+  ~io_context() = default;
 
   io_context(io_context const&) = delete;
   auto operator=(io_context const&) -> io_context& = delete;
   io_context(io_context&&) = delete;
   auto operator=(io_context&&) -> io_context& = delete;
 
-  auto run() -> std::size_t;
-  auto run_one() -> std::size_t;
-  auto run_for(std::chrono::milliseconds timeout) -> std::size_t;
+  auto run() -> std::size_t { return impl_->run(); }
+  auto run_one() -> std::size_t { return impl_->run_one(); }
+  auto run_for(std::chrono::milliseconds timeout) -> std::size_t { return impl_->run_for(timeout); }
 
-  void stop();
-  void restart();
-  auto stopped() const noexcept -> bool;
+  void stop() { impl_->stop(); }
+  void restart() { impl_->restart(); }
+  auto stopped() const noexcept -> bool { return impl_->stopped(); }
 
   /// Get an io_executor associated with this io_context
-  auto get_executor() noexcept -> io_executor;
+  auto get_executor() noexcept -> io_executor { return io_executor{*impl_}; }
 
  private:
   std::unique_ptr<detail::io_context_impl> impl_;
