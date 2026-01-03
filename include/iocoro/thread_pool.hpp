f3bacd9789762c4e4e7c0f2a63688376e735ceb8
yxzchen <chenyxmr@gmail.com>
2026-01-03 15:21:52 +0800

    sync

--- include/iocoro/thread_pool.hpp
+++ include/iocoro/thread_pool.hpp
@@ -107,23 +107,26 @@ class thread_pool::basic_executor_type {
       return;
     }
 
+    bool run_inline = false;
     {
       std::scoped_lock lock{state_->mutex};
 
-      // Inline run new tasks if stopped
       if (state_->stopped.load(std::memory_order_acquire)) {
-        detail::executor_guard g{any_executor{*this}};
-        f();
-        return;
+        run_inline = true;
+      } else {
+        state_->tasks.emplace([ex = *this, fn = std::forward<F>(f)]() mutable {
+          detail::executor_guard g{any_executor{ex}};
+          fn();
+        });
       }
-
-      state_->tasks.emplace([ex = *this, fn = std::forward<F>(f)]() mutable {
-        detail::executor_guard g{any_executor{ex}};
-        fn();
-      });
     }
 
-    state_->cv.notify_one();
+    if (run_inline) {
+      detail::executor_guard g{any_executor{*this}};
+      f();
+    } else {
+      state_->cv.notify_one();
+    }
   }
 
   template <class F>
